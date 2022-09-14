127d0e98f165df42bbb45dfa0bb178728c462e52
Daniel Belo Goncalves <danielg@theodo.co.uk>
2022-09-14 11:35:09 +0100

    Add prismic

---
+++ .slicemachine/assets/slices/News/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/News';
+
+export default {
+  title: 'slices/News'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Operation","spans":[]}],"description":[{"type":"paragraph","text":"Sunt ut mollit occaecat fugiat reprehenderit reprehenderit reprehenderit esse nulla.","spans":[]}]},"slice_type":"news","id":"_Default"}} />
+_Default.storyName = ''
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-import type { TypeA } from '../types'
+// import { SliceZone } from '@prismicio/react'
 
-const Example: TypeA = {
-  name: 'next',
+// import { createClient } from '../prismicio'
+// import { components } from '../slices'
+
+const Page = ({ page, navigation, settings }) => {
+  // return <SliceZone slices={page.data.slices} components={components} />
+  return <h1>Next</h1>
+  // return <SliceZone slices={page.data.slices} />
 }
 
-export default function Home() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <h1>
-        <p>{Example.name}</p>
-      </h1>
-    </div>
-  )
-}
+export default Page
+
+// export async function getStaticProps({ previewData }) {
+//   const client = createClient({ previewData })
+
+//   const page = await client.getSingle('homepage')
+
+//   return {
+//     props: {
+//       page,
+//     },
+//   }
+// }
